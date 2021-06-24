0558c7b120fa793aec608bb12e0b0b147cbe1192
Johan van de Merwe <j.vd.merwe@enovision.net>
2021-06-24 09:42:22 +0200

    2.2.9

--- src/singleton/LineIcons.js
+++ src/singleton/LineIcons.js
@@ -1,187 +1,251 @@
-/**
- * see: https://icons8.com/line-awesome
- */
 Ext.define('FontLineAwesome.singleton.LineIcons', {
-
     singleton: true,
     alternateClassName: ['Licons', 'Licon', 'LineIcons'],
 
     $configStrict: false, // surpresses warnings
 
     config: {
-        diamond: 'las la-gem',
-        gem: 'las la-gem',
-        link: 'las la-link',
-        unlink: 'las la-unlink',
-        undo: 'las la-undo-alt',
-        settings: 'las la-cog',
-        industry: 'las la-industry',
-        factory: 'las la-industry',
-        wrench: 'las la-wrench',
-        stock: 'las la-warehouse',
-        shoppingCart: 'las la-shopping-cart',
-        table: 'las la-table',
-        pin: 'las la-thumbtack',
-        tasks: 'las la-tasks',
+        add: 'las la-plus',
+        addresscard: 'las la-address-card',
+        archive: 'las la-archive',
+        backspace: 'las la-backspace',
+        barcode: 'las la-barcode',
+        bell: 'las la-bell',
+        broom: 'las la-broom',
+        bug: 'las la-bug',
+        building: 'las la-hotel',
+        bullseye: 'las la-bullseye',
+        calendar: 'las la-calendar',
+        calculator: 'las la-calculator',
+        camera: 'las la-camera',
+        cancel: 'las la-ban',
+        chalkboard: 'las la-chalkboard',
+        chartBar: 'las la-chart-bar',
+        chartLine: 'las la-chart-line',
+        chartPie: 'las la-chart-pie',
+        check: 'las la-check',
+        child: 'las la-child',
+        circle: 'las la-circle',
+        clipboard: 'las la-clipboard',
+        clock: 'las la-clock',
+        close: 'las la-times-circle',
+        cloud: 'las la-cloud',
+        code: 'las la-code',
         cog: 'las la-cog',
         cogs: 'las la-cogs',
-        new: 'las la-plus',
-        add: 'las la-plus',
-        edit: 'las la-edit',
-        save: 'las la-save',
-        archive: 'las la-archive',
-        page: 'las la-file-alt',
-        dot: 'lab la-first-order-alt',
-        bullseye: 'las la-bullseye',
-        star: 'las la-star',
-        keyboard: 'las la-keyboard',
-        circle: 'las la-circle',
-        camera: 'las la-camera',
-        dashboard: 'las la-tachometer-alt',
-        question: 'las la-question-circle',
-        help: 'las la-question-circle',
-        history: 'las la-history',
-        delete: 'las la-trash-alt',
-
-        microchip: 'las la-microchip',
-        puzzle: 'las la-puzzle-piece',
-
-        software: 'las la-cubes',
-        website: 'las la-globe',
-        legal: 'las la-balance-scale',
-        security: 'las la-shield-alt',
-        key: 'las la-key',
-        lock: 'las la-lock',
-        lockOpen: 'las la-lock-open',
-
-        download: 'las la-cloud-download-alt',
-        upload: 'las la-cloud-upload-alt',
-
-        info: 'las la-info-circle',
-        filter: 'las la-filter',
         comment: 'las la-comment',
-
-        database: 'las la-database',
-
-        note: 'las la-comment-alt',
-
-        document: 'las la-file-alt',
-        file: 'las la-file-alt',
-        fileWord: 'las la-file-word',
-        filePdf: 'las la-file-pdf',
-        fileImage: 'las la-file-image',
-        fileVideo: 'las la-file-video',
-        fileExcel: 'las la-file-excel',
-
-        video: 'las la-video',
-        send: 'las la-paper-plane',
-        move: 'las la-arrows-alt',
-        search: 'las la-search',
+        concept: 'las la-swatchbook',
+        copy: 'las la-copy',
         credits: 'las la-trophy',
-        calendar: 'las la-calendar',
-        clock: 'las la-clock',
-
+        cross: 'las la-times',
+        cubes: 'las la-cubes',
+        danger: 'las la-exclamation-triangle',
+        dashboard: 'las la-tachometer-alt',
+        database: 'las la-database',
+        delete: 'las la-trash-alt',
+        design: 'las la-swatchbook',
+        diamond: 'las la-gem',
+        divide: 'las la-divide',
+        document: 'las la-file-alt',
+        dot: 'lab la-first-order-alt',
+        download: 'las la-cloud-download-alt',
+        downloadFile: 'las fa-file-download',
+        drawingboard: 'las la-swatchbook',
+        edit: 'las la-edit',
+        email: 'las la-envelope',
+        equals: 'las la-equals',
+        exclamation: 'las la-exclamation',
+        factory: 'las la-industry',
+        female: 'las la-female',
+        femaleVenus: 'las la-venus',
+        file: 'las la-file-alt',
+        fileExcel: 'las la-file-excel',
+        fileImage: 'las la-file-image',
+        filePdf: 'las la-file-pdf',
+        fileVideo: 'las la-file-video',
+        fileWord: 'las la-file-word',
+        filter: 'las la-filter',
         flag: 'las la-flag',
         flagFinish: 'las la-flag-checkered',
-
-        mouse: 'las la-mouse',
-        cross: 'las la-times',
-        tick: 'las la-check',
-        check: 'las la-check',
-        ok: 'las la-check',
-        view: 'las la-eye',
-        cancel: 'las la-ban',
-        wait: 'las la-hourglass',
+        flask: 'las la-flask',
+        folder: 'las la-folder',
+        folderOpen: 'las la-folder-open',
+        frown: 'las la-frown',
+        gem: 'las la-gem',
+        genderless: 'las la-genderless',
+        handshake: 'las la-handshake',
+        happy: 'las la-smile',
+        help: 'las la-question-circle',
+        history: 'las la-history',
         home: 'las la-home',
-        trash: 'las la-trash',
-        recycle: 'las la-recycle',
-        close: 'las la-times-circle',
-
-        protect: 'las la-user-shield',
-
+        hourglass: 'las la-hourglass',
+        import: 'las la-file-import',
+        industry: 'las la-industry',
+        info: 'las la-info-circle',
+        key: 'las la-key',
+        keyboard: 'las la-keyboard',
+        layers: 'las la-layer-group',
+        legal: 'las la-balance-scale',
+        link: 'las la-link',
+        lightning: 'las la-bolt',
+        list: 'las la-list',
+        lock: 'las la-lock',
+        lockOpen: 'las la-lock-open',
         login: 'la-sign-in-alt',
         logout: 'las la-sign-out-alt',
-
-        powerOff: 'las la-power-off',
-
-        maleMars: 'las la-mars',
+        magnet: 'las la-magnet',
         male: 'las la-male',
-        femaleVenus: 'las la-venus',
-        female: 'las la-female',
-        transgender: 'las la-transgender',
-        genderless: 'las la-genderless',
-
-        email: 'las la-envelope',
-        print: 'las la-print',
-        copy: 'las la-copy',
-        paste: 'las la-paste',
-
-        thumbUp: 'las la-thumbs-up',
-        thumbDown: 'las la-thumbs-down',
-        handshake: 'las la-handshake',
-
-        teacher: 'las la-chalkboard-teacher',
-        student: 'las la-graduation-cap',
-        chalkboard: 'las la-chalkboard',
-        report: 'las la-chalkboard',
-        monitor: 'las la-chalkboard',
-
-        smile: 'las la-smile',
-        frown: 'las la-frown',
-
-        phone: 'las la-phone',
+        maleMars: 'las la-mars',
+        map: 'las la-map',
+        media: 'las la-photo-video',
+        microchip: 'las la-microchip',
+        minus: 'las la-minus',
         mobile: 'las la-mobile',
-
-        user: 'las la-user',
-        users: 'las la-users',
-        userFriends: 'las la-user-friends',
-        userTimes: 'las la-user-times',
-        userSecret: 'las la-user-secret',
-        userProtect: 'las la-user-shield',
-        child: 'las la-child',
-
-        previous: 'las la-arrow-left',
+        monitor: 'las la-desktop',
+        moon: 'las la-moon',
+        mouse: 'las la-mouse',
+        move: 'las la-arrows-alt',
+        movie: 'las la-film',
+        music: 'las la-music',
+        new: 'las la-plus',
+        newspaper: 'las la-newspaper',
         next: 'las la-arrow-right',
+        note: 'las la-comment-alt',
+        ok: 'las la-check',
+        page: 'las la-file-alt',
+        paste: 'las la-paste',
+        phone: 'las la-phone',
+        pin: 'las la-thumbtack',
+        playerBackward:  'las la-backward',
+        playerEject: 'las la-eject',
+        playerFastBackward:  'las la-fast-backward',
+        playerFastForward:  'las la-fast-forward',
+        playerFirst:  'las la-step-backward',
+        playerForward:  'las la-forward',
+        playerLast: 'las la-step-forward',
+        playerPause: 'las la-pause',
+        playerPlay:  'las la-play',
+        playerStop: 'las la-stop',
+        plug: 'las la-plug',
+        plus: 'las la-plus',
+        powerOff: 'las la-power-off',
+        previous: 'las la-arrow-left',
+        print: 'las la-print',
+        protect: 'las la-user-shield',
+        puzzle: 'las la-puzzle-piece',
+        qrcode: 'las la-qrcode',
+        question: 'las la-question-circle',
+        recycle: 'las la-recycle',
+        reminder: 'las la-bell',
+        replace: 'las la-retweet',
+        report: 'las la-chalkboard',
+        reset: 'las la-undo-alt',
+        refresh: 'las la-sync',
+        ringer: 'las la-bell',
+        sad: 'las la-frown',
+        save: 'las la-save',
+        school: 'las la-school',
+        search: 'las la-search',
+        security: 'las la-shield-alt',
+        select: 'las la-check-square',
+        send: 'las la-paper-plane',
+        settings: 'las la-cog',
+        shoppingCart: 'las la-shopping-cart',
+        smile: 'las la-smile',
+        snowflake: 'las la-snowflake',
+        software: 'las la-cubes',
+        sort: 'las la-sort',
+        sortAlphaDown: 'las la-sort-alpha-up',
+        sortAlphaUp: 'las la-sort-alpha-up',
+        sortAmount_asc: 'las la-sort-amount-up',
+        sortAmount_desc: 'las la-sort-amount-down',
+        sortDown: 'las la-sort-down',
+        sortNumDown: 'las la-sort-numeric-down',
+        sortNumUp: 'las la-sort-numeric-up',
+        sortUp: 'las la-sort-up',
+        stamp: 'las la-stamp',
+        star: 'las la-star',
+        starHalf: 'las la-star-half',
+        stock: 'las la-warehouse',
+        stop: 'las la-stop',
+        stopwatch: 'las la-stopwatch',
+        student: 'las la-graduation-cap',
+        sun: 'las la-sun',
+        sync: 'las la-sync',
+        table: 'las la-table',
+        tasks: 'las la-tasks',
+        teacher: 'las la-chalkboard-teacher',
+        template: 'las la-clipboard',
+        terminal: 'las la-terminal',
+        thumbDown: 'las la-thumbs-down',
+        thumbUp: 'las la-thumbs-up',
+        tick: 'las la-check',
+        times: 'las la-times',
+        toggleOff: 'las la-toggle-off',
+        toggleOn: 'las la-toggle-on',
+        transgender: 'las la-transgender',
+        transfer: 'las la-exchange-alt',
+        transport: 'las la-dolly',
+        trash: 'las la-trash',
+        truck: 'las la-truck',
+        truckLoading: 'las la-truck-loading',
+        tv: 'las la-tv',
+        undo: 'las la-undo-alt',
+        university: 'las la-university',
+        unlink: 'las la-unlink',
+        upload: 'las la-cloud-upload-alt',
+        uploadFile: 'las la-file-upload',
+        user: 'las la-user',
+        userCheck: 'las la-user-check',
+        userClock: 'las la-user-clock',
+        userEdit: 'las la-user-edit',
+        userFriends: 'las la-user-friends',
+        userLock: 'las la-user-lock',
+        userMinus: 'las la-user-minus',
+        userPlus: 'las la-user-plus',
+        userProtect: 'las la-user-shield',
+        userSecret: 'las la-user-secret',
+        userShield: 'las la-user-shield',
+        userTimes: 'las la-user-times',
+        users: 'las la-users',
+        vcard: 'las la-address-card',
+        video: 'las la-video',
+        view: 'las la-eye',
+        volumeDown: 'las la-volume-down',
+        volumeMute: 'las la-volume-off',
+        volumeUp: 'las la-volume-up',
+        wait: 'las la-hourglass',
+        website: 'las la-globe',
+        windowMinimize: 'las la-window-minimize',
+        windowMaximize: 'las la-window-maximize',
+        windowClose: 'las la-window-close',
+        wrench: 'las la-wrench',
         zoomIn: 'las la-search-plus',
         zoomOut: 'las la-search-minus',
 
-        sort: 'las la-sort',
-        sortUp: 'las la-sort-up',
-        sortDown: 'las la-sort-down',
-        sortAlphaUp: 'las la-sort-alpha-up',
-        sortAlphaDown: 'las la-sort-alpha-up',
-        sortNumUp: 'las la-sort-numeric-up',
-        sortNumDown: 'las la-sort-numeric-down',
-        sortAmount_asc: 'las la-sort-amount-up',
-        sortAmount_desc: 'las la-sort-amount-down',
-
-        toggleOn: 'las la-toggle-on',
-        toggleOff: 'las la-toggle-off',
-
-        chartPie: 'las la-chart-pie',
-        chartLine: 'las la-chart-line',
-        chartBar: 'las la-chart-bar',
-        flask: 'las la-flask',
-
-        folder: 'las la-folder',
-        folderOpen: 'las la-folder-open',
-        list: 'las la-list',
-
-        bell: 'las la-bell',
-        ringer: 'las la-bell',
-        reminder: 'las la-bell',
-
         // colors
-        red: '#f60202',
-        green: '#307203',
-        blue: '#05479a',
-        yellow: '#746601',
+        aqua: '#00FFFF',
         black: '#000',
-        white: '#fff',
-        gray: '#6d6d6d',
-        grayDark: '#3d3d3d',
-        orange: '#ff9900',
-        purple: '#9b00bf',
+        blue: '#05479A',
+        crimson: '#ce3d3d',
+        fuchsia: '#FF00FF',
+        gold: '#fae204',
+        gray: '#808080',
+        grayDark: '#3D3D3D',
+        green: '#008000',
+        lime: '#00FF00',
+        lightSteelBlue: '#9fc0e9',
+        maroon: '#800000',
+        navy: '#000080',
+        olive: '#808000',
+        orange: '#FF9900',
+        purple: '#800080',
+        red: '#FF0000',
+        sandyBrown: '#f6e14a',
+        silver: '#C0C0C0',
+        teal: '#008080',
+        white: '#FFF',
+        yellow: '#fcff40'
     },
 
     constructor: function (config) {
@@ -202,7 +266,6 @@ Ext.define('FontLineAwesome.singleton.LineIcons', {
                 me.setColoredIcons(source);
             }
         });
-
     },
 
     /**
@@ -229,22 +292,35 @@ Ext.define('FontLineAwesome.singleton.LineIcons', {
                 me.setIcon(`${prop}Color`, prop, color);
 
             } else {
+                if (typeof(me[baseProp]) === 'undefined') {
+                    Ext.log({
+                        msg: `Non existing Baseprop: ${baseProp} for Prop: ${prop}`,
+                        level: 'error',
+                        dump: me,
+                        stack: true
+                    });
+                } else {
 
-                xtIcon = me[baseProp]();
+                    xtIcon = me[baseProp]();
 
-                if (xtIcon) {
-                    xtSize = size === null ? 'la-lg' : `la-${size}`;
-                    xtClass = cls === null ? xtIcon : cls;
-                    xtColor = color === null ? '' : `style="color:${color};"`;
-                    xtValue = `<i class="la-${prop} ${xtClass} ${xtSize}" ${xtColor}></i>`;
+                    if (xtIcon) {
+                        xtSize = size === null ? 'la-lg' : `la-${size}`;
+                        xtClass = cls === null ? xtIcon : cls;
+                        xtColor = color === null ? '' : `style="color:${color};"`;
+                        xtValue = `<i class="la-${prop} ${xtClass} ${xtSize}" ${xtColor}></i>`;
 
-                    if (!me.hasOwnProperty(prop)) {
-                        me.setConfig(prop, () => xtValue);
-                    }
+                        if (!me.hasOwnProperty(prop)) {
+                            if (me.deployColoredIcons) {
+                                me.setConfig(prop, () => xtIcon);
+                            } else {
+                                me.setConfig(prop, () => xtValue);
+                            }
+                        }
 
-                    if (color === null) {
-                        if (me.deployColoredIcons) {
-                            me.setColoredIcons(prop);
+                        if (color === null) {
+                            if (me.deployColoredIcons) {
+                                me.setColoredIcons(prop);
+                            }
                         }
                     }
                 }
